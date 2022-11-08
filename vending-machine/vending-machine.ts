c1ff561232510a739e1cffe9cdb0b450bc6bac5b
Allison Ause <allison.ause@gmail.com>
2022-11-08 15:18:06 -0800

    added options for jsdoc --help command

--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -1,21 +1,33 @@
+/**
+ * @file Houses the vendingMachine function and the code that properly configures its arguments, taken from the command line.
+ */
+
 import process from 'node:process'
 
+/** Initializing global variables for consumption by following While code and to be used as arguments when vendingMachine is invoked  */
 let arg = null
 let cost = null
 let payment = null
 
-// Preliminary setup for parsing args from command line:
+/** Converts command line inputs of --item-cost and --payment from dollars into cents, ready for consumption by vendingMachine */
 while ((arg = process.argv.shift()) != null) {
   if (arg == '--item-cost') {
     const costInput = process.argv.shift()
     cost = Math.floor(Number(costInput || '0') * 100)
   } else if (arg == '--payment') {
     const paymentInput = process.argv.shift()
-    payment = Math.floor(Number(paymentInput || '0') * 100) // this means we're talking about cents instead of dollars.
+    payment = Math.floor(Number(paymentInput || '0') * 100)
+  } else if (arg === '--help') {
+    // show user what arguments are required and their format
+    console.error(`Options:
+    --item-cost <value> value is a number for cost of desired item in dollar decimal format
+    --payment <value>   value is a number for the payment provided in dollar decimal format
+    `)
+    process.exit(0)
   }
 }
 
-/** Error handling in place for a lack of arguments (either --item-cost or ) entered into the command line. */
+/** Error handling in place for scenario wherein arguments (either --item-cost or --payment or both) are not entered into the command line, prompting an error code and exit. */
 if (cost == null) {
   console.error('--item-cost is required but not provided. Exiting')
   process.exit(1)
@@ -30,16 +42,20 @@ let q = 25
 let d = 10
 let n = 5
 
-/** The vendingMachine function takes a costInput (calculated from the --item-cost arg from the command line) and a paymentInput (calculated from the --payment arg from the command line) and returns the itemized change of American currency in named coin denominations along with the calculated total change returned after the cost is subtracted from the payment. Returns message of insufficient funds if cost is greater than payment. */
-function vendingMachine(costInput: number, paymentInput: number): void {
+/** The vendingMachine function takes a itemCost (calculated from the --item-cost arg from the command line) and a paid (calculated from the --payment arg from the command line) and returns the itemized change of American currency in named coin denominations along with the calculated total change returned after the cost is subtracted from the payment. Returns message of insufficient funds if cost is greater than payment.
+ *  @param {number} itemCost The amount of money it takes to purchase this item.
+ *  @param {number} amountPaid The amount of money the user has inserted in order to purchase this item.
+ * @returns {void} console.log of the response string that includes an assembly of relevant information about how many coins are returned and in what amounts, along with total change.
+ */
+function vendingMachine(itemCost: number, amountPaid: number): void {
   let response = ''
-  if (paymentInput < costInput) {
+  if (amountPaid < itemCost) {
     response += `Insufficient funds for desired product. We have returned your $${
-      paymentInput / 100
+      amountPaid / 100
     }`
     return console.log(response)
   }
-  const totalChange = paymentInput - costInput
+  const totalChange = amountPaid - itemCost
   const Quarters = Math.floor(totalChange / q)
   if (Quarters > 0) {
     response += `Quarters: ${Quarters} \n`
@@ -63,11 +79,5 @@ function vendingMachine(costInput: number, paymentInput: number): void {
   return console.log(response)
 }
 
+/** Invoking vendingMachine function for use via command line */
 vendingMachine(cost, payment)
-
-// Answer:
-// totalChange: 305c
-// Quarters: 12
-// Dimes: 0
-// Nickels: 1
-// Pennys: 0
